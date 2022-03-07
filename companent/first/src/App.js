06ff2e6197e87048e4e063625c6f6cd4a136a8fb
fatihozkul35 <fatihozkul3541@gmail.com>
2022-03-07 17:09:03 +0100

    today ı learned myself Components in react

---
+++ companent/first/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header";
+function App() {
+  return (
+    <div>
+      <h1>Hello React i came </h1>
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ companent/first/src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div>
+      <header>I'm a header element</header>
+    </div>
+  );
+}
+
+export default Header;
